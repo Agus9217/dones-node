036e7cb0e3e3c8c7fca6eff2ef072316a49d3273
Agus9217 <justinibarrola92@gmail.com>
2024-11-05 13:51:34 -0300

    Haciendo peticiones al servidor mongo

--- src/v1/controllers/disfruto.controller.ts
+++ src/v1/controllers/disfruto.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
+import { Checklist } from "../models";
 
-export const disfrutoGetAllController = async (_req: Request, res: Response) => {
-  res.send('Hello from Controller')
+export const disfrutoGetAllController = async (req: Request, res: Response) => {
+  const data = await Checklist.find({})
+  res.send({data: data})
+}
+
+export const disfrutoPostController = async (req: Request, res: Response) => {
+  console.log(req)
+  res.send({ data: 'Hola desde el post' })
 }
--- src/v1/routes/router.ts
+++ src/v1/routes/router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { disfrutoGetAllController } from "../controllers/disfruto.controller";
+import { disfrutoGetAllController, disfrutoPostController } from "../controllers/disfruto.controller";
 
 export const router = Router()
 
 router
   .get('/', disfrutoGetAllController)
+  .post('/', disfrutoPostController)
